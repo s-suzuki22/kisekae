6d1aa6a1d9a7cf433001f332728a3e362e42732b
s-suzuki22 <nixe.suzuki0220@gmail.com>
2025-09-22 14:22:31 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,69 @@ function App() {
 
   return (
     <div style={{ textAlign: 'center', padding: '1rem' }}>
-      <h1>きせかえゲーム</h1>
+      <h1 style={{ fontSize: '2rem', color: 'hotpink' }}>🌸 きせかえゲーム 🌸</h1>
 
-      <div style={{ position: 'relative', width: '200px', margin: '0 auto' }}>
-        <img src="/images/base.png" alt="base" style={{ width: '100%' }} />
-        <img src={`/images/${hair}`} alt="hair" style={{ position: 'absolute', top: 0, left: 0, width: '100%' }} />
-        <img src={`/images/${dress}`} alt="dress" style={{ position: 'absolute', top: 0, left: 0, width: '100%' }} />
-        <img src={`/images/${accessory}`} alt="accessory" style={{ position: 'absolute', top: 0, left: 0, width: '100%' }} />
+      {/* キャラクター表示エリア */}
+      <div style={{ position: 'relative', width: '300px', margin: '0 auto' }}>
+        {/* 素体 */}
+        <img
+          src="/images/base.png"
+          alt="base"
+          style={{ width: '100%', display: 'block', zIndex: 1 }}
+        />
+        {/* 髪型 */}
+        <img
+          src={`/images/${hair}`}
+          alt=""
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            zIndex: 2,
+          }}
+        />
+        {/* 服 */}
+        <img
+          src={`/images/${dress}`}
+          alt=""
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            zIndex: 3,
+          }}
+        />
+        {/* アクセサリ */}
+        <img
+          src={`/images/${accessory}`}
+          alt=""
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            zIndex: 4,
+          }}
+        />
       </div>
 
+      {/* 髪型選択 */}
       <div style={{ marginTop: '1rem' }}>
         <h2>髪型</h2>
         <button onClick={() => setHair('hair1.png')}>髪1</button>
         <button onClick={() => setHair('hair2.png')}>髪2</button>
       </div>
 
+      {/* 服選択 */}
       <div style={{ marginTop: '1rem' }}>
         <h2>服</h2>
         <button onClick={() => setDress('dress1.png')}>服1</button>
         <button onClick={() => setDress('dress2.png')}>服2</button>
       </div>
 
+      {/* アクセサリ選択 */}
       <div style={{ marginTop: '1rem' }}>
         <h2>アクセサリ</h2>
         <button onClick={() => setAccessory('accessory1.png')}>アクセ1</button>
@@ -38,3 +80,4 @@ function App() {
 }
 
 export default App
+
